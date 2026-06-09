f0115aba1737b2fcc18b87f99ad44a97ac24f3a9
cristianpuris-bit <cristian.puris@tecsup.edu.pe>
2026-06-08 19:20:09 -0500

    Update Ejercicio12.cs

--- SCRIPS/Ejercicio12.cs
+++ SCRIPS/Ejercicio12.cs
@@ -3,16 +3,27 @@ using UnityEngine;
 
 public class Ejercicio12 : MonoBehaviour
 {
-    int[] dañoTotal = new int[4] { 20, 25, 30, 35 };
+    int[] daño = new int[4] { 20, 25, 30, 35 };
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int index = 0;  
+       int i = 0;
+        int valor  = 0;
+        while (i < daño.Length)
+        {
+            Debug.Log("hiciste " + daño[i] + " en total ");
+            valor += daño[i];
+                  
+            i++;
+
+        }
+
+        Debug.Log("hiciste en total de " + valor);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
